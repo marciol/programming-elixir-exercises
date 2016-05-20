07bc9155f65378b1dbc2b5a62d34a78d3492d4d2
Mááarcio de Faria <marcitolopes@gmail.com>
2016-05-20 07:11:04 -0600

    completed capitalize_sentences

--- ch-11/strings_and_binaries-06.exs
+++ ch-11/strings_and_binaries-06.exs
@@ -1,9 +1,23 @@
 defmodule StringsAndBinaries do
-  dep capitalize_sentences(str)
+  def capitalize_sentences(str)
     when is_binary(str), do: _capitalize_sentences(str)
-    
-  
-  
-  defp capitalize_sentences(<< h :: utf8, t :: binary >>)
-    when 
+ 
+  defp _capitalize_sentences(str) do 
+    _split_sentences(str) |> _capitalize |> Enum.reverse |> Enum.join(" ")
+  end
+
+  defp _split_sentences(str, s \\ "", ss \\ [])
+
+  defp _split_sentences(<< h :: utf8, t :: binary >>, s, ss)
+    when h == ?. do 
+      s = String.reverse(<< h :: utf8, s :: binary >>) |> String.strip
+      _split_sentences(t, "", [ s | ss ])
+  end
+
+  defp _split_sentences(<< h :: utf8, t :: binary >>, s, ss), 
+    do: _split_sentences(t, << h :: utf8, s :: binary >>, ss)
+
+  defp _split_sentences(<<>>, _, ss), do: ss
+
+  defp _capitalize(ss), do: ss |> Enum.map(&String.capitalize/1)
 end
